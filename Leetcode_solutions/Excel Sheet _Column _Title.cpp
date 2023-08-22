25758358b2eccb0919b29b87124586e0eb06825c
Pallavi rampal <pallavirampal30@gmail.com>
2023-08-22 20:36:12 +0530

    Excel Sheet Column Title .cpp

---
+++ Leetcode_solutions/Excel Sheet _Column _Title.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans;
+        
+        while(n){
+            ans.insert(ans.begin(), 'A' + (n-1)%26);
+            n = (n-1)/26;
+        }
+        
+        return ans;
+    }
+};
